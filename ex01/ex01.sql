fd0c9d38fbe808a30983ff2ec8d5852b84f2855e
Brice Waegeneire <brice.wge@gmail.com>
2017-06-19 11:19:13 +0200

    ex01

---
+++ ex01/ex01.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `db_bwaegene`.`ft_table` (
+       `id` int NOT NULL AUTO_INCREMENT,
+       `login` varchar(8) NOT NULL DEFAULT 'toto',
+       `group` enum('staff', 'student', 'other') NOT NULL,
+       `creation_date` date NOT NULL,
+       PRIMARY KEY (id)
+       );

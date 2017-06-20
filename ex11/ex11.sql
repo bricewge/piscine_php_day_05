89a6b79cd19a9236a3c8bb6647f3d2e6e54ec915
Brice Waegeneire <brice.wge@gmail.com>
2017-06-20 17:54:26 +0200

    ex11-ex13

---
+++ ex11/ex11.sql
@@ -0,0 +1,8 @@
+SELECT last_name AS `NAME`, first_name, price
+FROM member
+INNER JOIN subscription
+ON member.id_sub = subscription.id_sub
+INNER JOIN user_card
+ON member.id_user_card = user_card.id_user
+WHERE price > 42
+ORDER BY last_name, first_name;
